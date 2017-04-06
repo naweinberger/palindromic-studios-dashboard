dbd3f51b842db97bf29caa3f717ff64354cc09d7
naweinberger <naweinberger@gmail.com>
2017-04-06 17:04:46 -0400

    Wrap EntryItem in Card

--- app/components/EntryItem.js
+++ app/components/EntryItem.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { ListItem } from 'material-ui/List'
+import { Card } from 'material-ui/Card'
 
 export class EntryItem extends React.Component {
 	render() {
 		let entry = this.props.entry
 		return (
-			<ListItem
-				primaryText={entry.food.name}
-				secondaryText={`${Math.round(entry.calories)} cal`}
-				containerElement={<Link to={`/entry/${entry.id}`} />} />
+			<Card style={{
+		      margin: 10
+		    }}>
+				<ListItem
+					primaryText={entry.food.name}
+					secondaryText={`${Math.round(entry.calories)} cal`}
+					containerElement={<Link to={`/entry/${entry.id}`} />}
+				/>
+			</Card>
 			)
 	}
 }
